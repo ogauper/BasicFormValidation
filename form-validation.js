ab298f54e064bfaab23e24f69f8f86a914bd692b
Unknown <ogauper@mail.csuchico.edu>
2017-03-07 11:43:40 -0800

    Fixed username/pw validation

--- form-validation.js
+++ form-validation.js
@@ -3,17 +3,37 @@ function validate(){
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
 
-  //Show message that there is an error with the username...
-  document.getElementById("usernameError").innerHTML="Bad username.";
-  document.getElementById("usernameError").classList.remove("hidden-message");
-  document.getElementById("usernameError").classList.add("shown-message");
-  //Turn the username items red
-  document.getElementById("usernameGroup").classList.add("has-error");
+  if(userEntered.length<6){
+    //Show message that there is an error with the username...
+    document.getElementById("usernameError").innerHTML="Bad username.";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+    //Turn the username items red
+    document.getElementById("usernameGroup").classList.add("has-error");
+  }
+  else if (userEntered.length>6) {
+    document.getElementById("usernameGroup").classList.add("has-success");
+    document.getElementById("usernameSuccess").innerHTML="Good Username.";
+    document.getElementById("usernameSuccess").classList.remove("hidden-message");
+    document.getElementById("usernameSuccess").classList.add("shown-message");
 
-  //Show message that there is an error with the password...
+  }
+
+
+  if(passEntered == "password"){
   document.getElementById("passwordError").innerHTML="Bad password.";
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
   //Turn the password items red
   document.getElementById("passwordGroup").classList.add("has-error");
+}
+  else{
+    document.getElementById("passwordSuccess").innerHTML="Good Password.";
+    document.getElementById("passwordSuccess").classList.remove("hidden-message");
+    document.getElementById("passwordSuccess").classList.add("shown-message");
+    document.getElementById("passwordGroup").classList.add("has-success");
+
+
+
+  }
 }
